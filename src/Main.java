6f445dae48e2ef40ea1ff51d04c67bcbaea6b215
shiviii123456 <shivangi1913087@akgec.ac.in>
2023-02-20 23:02:48 +0530

    frst

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        //Game Start
         GameLauncher game=new GameLauncher();
         game.start();
+
     }
 }
