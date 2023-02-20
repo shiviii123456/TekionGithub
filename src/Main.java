f9b288b0a9fc9a7b941e8f75c36ef97047d4c595
shiviii123456 <70109170+shiviii123456@users.noreply.github.com>
2023-02-21 00:02:02 +0530

    frst

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        //Game launcher
-        //Game starter
-        GameLauncher game=new GameLauncher();
+
+        GameLauncher game = new GameLauncher();
         game.start();
 
     }
--- src/ScoreBoard.java
+++ src/ScoreBoard.java
@@ -1,15 +1,13 @@
 public class ScoreBoard {
-    public void scoreBoard(Teams team1,Teams team2){
-        System.out.println("Total score of "+team1.getName()+" "+team1.getScore()+" at wickets "+team1.getWickets()+"/11");
-        System.out.println("Total score of "+team2.getName()+" "+team2.getScore()+" at wickets "+team2.getWickets()+"/11");
-        if(team1.getScore()>team2.getScore()){
-            System.out.println(team1.getName()+" has won the match by "+(team1.getScore()-team2.getScore())+" runs ");
-        }
-        else if(team1.getScore()==team2.getScore()){
-            System.out.println("Tie between "+team1.getName()+" and "+team2.getName());
-        }
-        else{
-            System.out.println(team2.getName()+" has won the match "+(team2.getScore()-team1.getScore())+" runs ");
+    public static void scoreBoard(Teams team1,Teams team2){
+        System.out.println("Total score of " + team1.getName() + " " + team1.getScore() + " at wickets " + team1.getWickets() + "/10");
+        System.out.println("Total score of " + team2.getName() + " " + team2.getScore() + " at wickets " + team2.getWickets() + "/10");
+        if (team1.getScore() > team2.getScore()) {
+            System.out.println(team1.getName() + " has won the match by " + (team1.getScore() - team2.getScore()) + " runs ");
+        } else if (team1.getScore() == team2.getScore()) {
+            System.out.println("Tie between " + team1.getName() + " and " + team2.getName());
+        } else {
+            System.out.println(team2.getName() + " has won the match " + (team2.getScore() - team1.getScore()) + " runs ");
         }
     }
 }
