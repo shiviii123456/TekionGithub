a5d6042b40cc696d737abf1fcfd33f431eb4c54f
Shivangi Singh <shivangis@tekion.com>
2023-02-27 12:23:50 +0530

    enum part

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-
+        //GameStart
         GameLauncher game = new GameLauncher();
         game.start();
 
